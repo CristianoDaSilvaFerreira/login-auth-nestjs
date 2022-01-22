e7c738fd41a8d517f1a5ca2b90cb5b14a43b4a92
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-01-22 10:12:31 -0300

    :rocket: implement creat

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  // Acesso o Prisma
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+
+    // Transformando a senha bruta em um hash
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    // Enviando as informações para o Prisma
+    const createUser = await this.prisma.user.create({ data });
+
+    return {
+      ...createUser,
+      password: undefined,
+    };
   }
 
   findAll() {
