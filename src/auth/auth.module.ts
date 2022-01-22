c32d0f7922ca578d45c97067d24627c97eecbe03
Cristiano Ferreira <cristianodevsystem@gmail.com>
2022-01-22 17:19:41 -0300

    :rocket: implement local strategy validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
+import { LocalStrategy } from './strategies/local.strategy';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    // Buscando o usuário no userService
+    const user = await this.userService.findByEmail(email);
+
+    // Verificando se encontrou o usuário
+    if (user) {
+      // Chegar se a senha informada corresponde a hash que está no banco
+
+      // Comparando a senha criptografaga com a senha do usuário
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      // Chegando se a senha é válida
+      if (isPasswordValid) {
+          return {
+              ...user,
+              password: undefined,
+          };
+      }
     }
+
+    // Se chegar aqui, significa que não encontrou um user e/ou senha não corresponde
+    throw new Error('E-mail ou senha estão incorretos, favor verificar e tente novamente!')
+  }
 }
